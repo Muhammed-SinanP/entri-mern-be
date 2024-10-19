c0e4804da6460c46e2fb7a6c85becda3a361902c
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2024-10-19 22:38:31 +0530

    change corsOptions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const authRoutes = require('./routes/authRoutes');
 const corsOptions = {
-  origin: 'https://e44sinan-mern-assignment.netlify.app',  
+  origin: ['http://localhost:5173','http://localhost:5000','https://e44sinan-mern-assignment.netlify.app'],  
   optionsSuccessStatus: 200,
 };
 
@@ -19,10 +19,15 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.error(err));
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!'); // Response for the root URL
+});
+
+
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
